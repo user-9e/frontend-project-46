6a05cfe267ac82a362716f2b9d1cc5ffa3c14d07
user-9e <barbara1brave@gmail.com>
2024-01-31 21:40:15 +0300

    update tests

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,21 @@
+
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import gendiff from '../src/gendiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+
+test('gendiff', () => {
+    // ../__fixtures__/file
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file2.json');
+    const result = gendiff(file1, file2);
+    expect(result).toEqual(readFile('output.txt'));
+  });
