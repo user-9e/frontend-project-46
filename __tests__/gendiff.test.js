50a6ed6d976cc511a21f104f7766c3253ef246e5
user-9e <barbara1brave@gmail.com>
2024-02-19 17:19:08 +0300

    add json output format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,14 +11,21 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 test('stylish nested gendiff', () => {
   const file1json = getFixturePath('file1.json');
-  const file2json = getFixturePath('file2.json');
-  const result = gendiff(file1json, file2json, 'stylish');
+  const file2yml = getFixturePath('file2.yml');
+  const result = gendiff(file1json, file2yml, 'stylish');
   expect(result).toEqual(readFile('output.txt'));
 });
 
 test('plain nested gendiff', () => {
   const file1yml = getFixturePath('file1.yml');
-  const file2yml = getFixturePath('file2.yml');
-  const result = gendiff(file1yml, file2yml, 'plain');
+  const file2json = getFixturePath('file2.json');
+  const result = gendiff(file1yml, file2json, 'plain');
   expect(result).toEqual(readFile('plain.txt'));
+});
+
+test('json nested gendiff', () => {
+  const file1json = getFixturePath('file1.json');
+  const file2yml = getFixturePath('file2.yml');
+  const result = gendiff(file1json, file2yml, 'json');
+  expect(result).toEqual(readFile('output.json'));
 });
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,11 @@
 import plain from './plain.js';
 import stylish from './stylish.js';
+import json from './json.js';
 
 export default (diff, format) => {
-  if (format === 'stylish') {
-    return stylish(diff);
+  if (format === 'json') {
+    return json(diff);
   } if (format === 'plain') {
     return plain(diff);
-  } return diff;
+  } else return stylish(diff);
 };
